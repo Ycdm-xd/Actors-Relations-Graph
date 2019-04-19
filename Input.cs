7905aebb21316dde411cf82d0fd7e2112184a333
Ycdm <youssefycdm@gmail.com>
2019-04-19 16:25:30 +0200

    Split Program.cs into Driver.cs and Graph.cs + Created Input.cs

---
+++ Input.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Graph;
+using Driver;
+
+namespace Input
+{
+    public static class MovieFile
+    {
+        private static List<string[]> allData = new List<string[]>();
+        private static void Get()
+        { 
+            FileStream fileStream = new FileStream("Movies.txt", FileMode.OpenOrCreate);
+            StreamReader streamReader = new StreamReader(fileStream);
+            while (!streamReader.EndOfStream)
+            {
+                allData.Add(streamReader.ReadLine().Split('/'));
+            }
+            streamReader.Close();
+            fileStream.Close();
+        }
+        private static void Set()
+        {
+            foreach (string[] data in allData)
+            {
+                for (int i = 1; i < data.Length; ++i)
+                {
+                    for (int j = i + 1; j < data.Length; ++j)
+                    {
+                        Edge.AddEdge(data[i], data[j], data[0]);
+                    }
+                }
+            }
+        }
+    }
+}
